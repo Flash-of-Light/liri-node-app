968d59803b7f82c85612e8de604be563643b7f3b
Jay Goss <jayspgoss@gmail.com>
2018-11-06 20:57:11 -0500

    change

--- liri.js
+++ liri.js
@@ -3,6 +3,11 @@ require("dotenv").config();
 //Make it so liri.js can take in one of the following commands:
 
 // concert-this
+
+https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp
+
+
+
 // spotify-this-song
 // movie-this
 // do-what-it-says
